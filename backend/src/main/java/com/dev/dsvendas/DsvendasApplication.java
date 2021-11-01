572d6bfc2eb716fe84a7851dfce4fa62f7b990fa
antonio <prof.antonioqsneto@gmail.com>
2021-11-01 19:07:36 -0300

    initial commit

---
+++ backend/src/main/java/com/dev/dsvendas/DsvendasApplication.java
@@ -0,0 +1,14 @@
+package com.dev.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+		System.out.println("Aplicação iniciada com sucesso!");
+	}
+
+}
