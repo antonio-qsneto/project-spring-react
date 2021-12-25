7d8dc1b9449352ef2e47501a09d36927c85dc884
antonio-qsneto <prof.antonioqsneto@gmail.com>
2021-12-25 17:01:43 -0300

    Security Config

--- backend/src/main/java/com/dev/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/dev/dsvendas/DsvendasApplication.java
@@ -9,6 +9,8 @@ public class DsvendasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsvendasApplication.class, args);
 		System.out.println("Aplicação iniciada com sucesso!");
+
+		
 	}
 
 }
